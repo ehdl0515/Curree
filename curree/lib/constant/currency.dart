7198444b33e9f78c98a0dabb6e596184c09b8a30
KDH <ehdl0515@naver.com>
2024-03-21 22:19:53 +0900

    [MOD (lib)] 통화 정보 클래스 수정
    - mainCurrency 는 한화 '원' 으로 고정하므로 변수제거
    - 각 통화별 정보를 모두 담도록 속성 추가

--- curree/lib/constant/currency.dart
+++ curree/lib/constant/currency.dart
@@ -1,32 +1,86 @@
 class Currency {
+  final String nation;
   final String code;
   final String name;
   final String symbol;
+  final String currentRate;
+  final int currentDate;
+  final String previousRate;
+  final int previousDate;
+  final String flagImage;
 
-  const Currency({
+  Currency({
+    required this.nation,
     required this.code,
     required this.name,
     required this.symbol,
+    required this.currentRate,
+    required this.currentDate,
+    required this.previousRate,
+    required this.previousDate,
+    required this.flagImage,
   });
 
   @override
   String toString() {
-    return '[${code}, ${name}, ${symbol}]';
+    return '[$code, $name, $symbol, ($currentRate, $currentDate), ($previousRate, $previousDate), $flagImage]';
   }
 
-  static const Currency won = Currency(code: "KRW", name: "won", symbol: "₩");
-  static const Currency yen = Currency(code: "JPY", name: "yen", symbol: "￥");
-  static const Currency dollar = Currency(code: "USD", name: "dollar", symbol: "\$");
+  static Currency dollar = Currency(
+      nation: "미국",
+      code: "USD",
+      name: "달러",
+      symbol: "\$",
+      currentRate: (0.0).toStringAsFixed(1),
+      currentDate: 20240321141000,
+      previousRate: (0.0).toStringAsFixed(1),
+      previousDate: 20240321140000,
+      flagImage: "assets/images/USAFlag.png",
+  );
+
+  static Currency euro = Currency(
+      nation: "유럽",
+      code: "EUR",
+      name: "유로",
+      symbol: "€",
+      currentRate: (0.0).toStringAsFixed(1),
+      currentDate: 20240321141000,
+      previousRate: (0.0).toStringAsFixed(1),
+      previousDate: 20240321140000,
+      flagImage: "assets/images/EuropeFlag.png",
+  );
+
+  static Currency yen = Currency(
+      nation: "일본",
+      code: "JPY",
+      name: "엔",
+      symbol: "￥",
+      currentRate: (0.0).toStringAsFixed(1),
+      currentDate: 20240321141000,
+      previousRate: (0.0).toStringAsFixed(1),
+      previousDate: 20240321140000,
+      flagImage: "assets/images/JapanFlag.png",
+  );
+
+  static Currency yuan = Currency(
+      nation: "중국",
+      code: "CNY",
+      name: "위안",
+      symbol: "元",
+      currentRate: (0.0).toStringAsFixed(1),
+      currentDate: 20240321141000,
+      previousRate: (0.0).toStringAsFixed(1),
+      previousDate: 20240321140000,
+      flagImage: "assets/images/ChinaFlag.png",
+  );
+
+
 }
 
-List<Currency> currencyList = [
-  Currency.won,
-  Currency.yen,
-  Currency.dollar,
-];
 
-List<String> currencyCodeList = [
-  Currency.won.code,
-  Currency.yen.code,
-  Currency.dollar.code,
+List<Currency> currencies = [
+  Currency.dollar,
+  Currency.euro,
+  Currency.yen,
+  Currency.yuan,
 ];
