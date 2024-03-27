91ba34395965738713db94d1f2d896d75a17d2db
KDH <ehdl0515@naver.com>
2024-03-27 15:53:25 +0900

    [MOD (lib)] device 정보 및 프로파일 업데이트 추가
    - device 정보 가져온 후, backend 에 요청하여
      프로파일 정보를 업데이트
    - 그 외 버그 수정

--- curree/lib/bottom_navigation_bar.dart
+++ curree/lib/bottom_navigation_bar.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
+import 'package:curree/constant/currency.dart';
 import 'package:curree/providers/logger_provider.dart';
+import 'package:curree/providers/rate_provider.dart';
+import 'package:curree/providers/setting_provider.dart';
 import 'package:curree/screens/currency_table_screen.dart';
 import 'package:curree/screens/current_rate_screen.dart';
 import 'package:curree/screens/setting_screen.dart';
+import 'package:curree/util/call_api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 class MyBottomNavigationBar extends StatefulWidget {
@@ -25,6 +32,7 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
 
   void _onItemTapped(int index) {
     setState(() {
+      context.read<RateProvider>().setSelectedNation(null);
       _selectedIndex = index;
     });
   }
@@ -33,19 +41,49 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
   @override
   Widget build(BuildContext context) {
     final logger = Provider.of<LoggerProvider>(context).logger;
-    logger.d('build MyBottomNavigationBar - selectedIndex: $_selectedIndex');
+    logger.i('MyBottomNavigationBar] build');
+
+    Map<String, dynamic> deviceInfo = context.read<SettingProvider>().deviceInfo;
+    logger.d("MyBottomNavigationBar] selectedIndex: $_selectedIndex, identifierForVendor: ${deviceInfo['identifierForVendor']}");
+
+    final backendServer = dotenv.get("BACKEND_SERVER", fallback: "127.0.0.1:8000");
+
+    Map<String, dynamic> params = {
+      "deviceId": deviceInfo["identifierForVendor"],
+    };
+    Uri url = Uri.http(backendServer, "/profile", params);
+    Future<String> result = callGetAPI(url);
+
+    result.then((newValueJson) {
+      dynamic newValue = jsonDecode(newValueJson);
+      logger.d("MyBottomNavigationBar] get response profileInfo: $newValue");
+      String subCurrencyCode = newValue[0]["subCurrency"];
+      Currency subCurrency = currencies[0];
+      for (var element in currencies) {
+        if (element.code == subCurrencyCode) {
+          subCurrency = element;
+          break;
+        }
+      }
+      context.read<SettingProvider>().setProfileNo(newValue[0]["profileNo"]);
+      context.read<SettingProvider>().setSubCurrency(subCurrency);
+      context.read<SettingProvider>().setExchangeMinimum(newValue[0]["exchangeMinimum"]);
+      context.read<SettingProvider>().setExchangeMaximum(newValue[0]["exchangeMaximum"]);
+      context.read<SettingProvider>().setExchangeIncreaseUnit(newValue[0]["exchangeIncreaseUnit"]);
+    });
+
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
-        title: Text("Curree"),
+        title: const Text("Curree", style: TextStyle(fontWeight: FontWeight.bold,),),
       ),
       body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
       bottomNavigationBar: BottomNavigationBar(
         selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
         type: BottomNavigationBarType.fixed,
--- curree/lib/main.dart
+++ curree/lib/main.dart
@@ -2,20 +2,25 @@ import 'package:curree/providers/filter_provider.dart';
 import 'package:curree/providers/logger_provider.dart';
 import 'package:curree/providers/setting_provider.dart';
 import 'package:curree/providers/rate_provider.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'bottom_navigation_bar.dart';
 
 
 
-void main() {
+Future<void> main() async {
 
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   final loggerProvider = LoggerProvider();
+
+  await dotenv.load(fileName: ".env");
 
   runApp(
       MultiProvider(
@@ -34,16 +39,101 @@ void main() {
 }
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatFormState();
+  }
+
+  Future<void> initPlatFormState() async {
+    var deviceData = <String, dynamic>{};
+    try {
+      if (defaultTargetPlatform == TargetPlatform.android) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      context.read<SettingProvider>().setDeviceInfo(deviceData);
+    });
+  }
+
+  Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, dynamic>{
+      'version.securityPatch': build.version.securityPatch,
+      'version.sdkInt': build.version.sdkInt,
+      'version.release': build.version.release,
+      'version.previewSdkInt': build.version.previewSdkInt,
+      'version.incremental': build.version.incremental,
+      'version.codename': build.version.codename,
+      'version.baseOS': build.version.baseOS,
+      'board': build.board,
+      'bootloader': build.bootloader,
+      'brand': build.brand,
+      'device': build.device,
+      'display': build.display,
+      'fingerprint': build.fingerprint,
+      'hardware': build.hardware,
+      'host': build.host,
+      'id': build.id,
+      'manufacturer': build.manufacturer,
+      'model': build.model,
+      'product': build.product,
+      'supported32BitAbis': build.supported32BitAbis,
+      'supported64BitAbis': build.supported64BitAbis,
+      'supportedAbis': build.supportedAbis,
+      'tags': build.tags,
+      'type': build.type,
+      'isPhysicalDevice': build.isPhysicalDevice,
+      'systemFeatures': build.systemFeatures,
+      'serialNumber': build.serialNumber,
+    };
+  }
+
+  Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, dynamic>{
+      'name': data.name,
+      'systemName': data.systemName,
+      'systemVersion': data.systemVersion,
+      'model': data.model,
+      'localizedModel': data.localizedModel,
+      'identifierForVendor': data.identifierForVendor,
+      'isPhysicalDevice': data.isPhysicalDevice,
+      'utsname.sysname:': data.utsname.sysname,
+      'utsname.nodename:': data.utsname.nodename,
+      'utsname.release:': data.utsname.release,
+      'utsname.version:': data.utsname.version,
+      'utsname.machine:': data.utsname.machine,
+    };
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
 
     final logger = Provider.of<LoggerProvider>(context).logger;
+    logger.d('Curree 앱을 시작합니다. ${DateTime.timestamp()}');
     if (kDebugMode) {
-      logger.d('Curree 앱을 시작합니다.');
-      logger.d(DateTime.timestamp());
       logger.d("디버그 모드입니다.");
     } else if (kReleaseMode) {
       logger.d("릴리즈 모드입니다.");
